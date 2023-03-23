efec39920de68f91a8c3d8da282a44edf5abed8c
EmreKaya0860 <h.emrekaya2207@gmail.com>
2023-03-23 15:59:09 +0300

    loginreg

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 const express = require("express");
 const app = express();
-const sqlite3 = require('sqlite3');
+const sqlite3 = require("sqlite3");
 
 app.use(express.json()); // Used to parse JSON bodies
-app.use(express.urlencoded({extended: true})); //Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
 
 //Connect to the database or create one if it doesn't exist
-var db = new sqlite3.Database('./db/userInfo.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err && err.code == "SQLITE_CANTOPEN") {
-    createUserDatabase()
+var db = new sqlite3.Database(
+  "./db/userInfo.db",
+  sqlite3.OPEN_READWRITE,
+  (err) => {
+    if (err && err.code == "SQLITE_CANTOPEN") {
+      createUserDatabase();
+    } else if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the user info database.");
   }
-  else if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the user info database.');
-});
+);
 
 //Creates a new database if one doesn't exist
 function createUserDatabase() {
-  var newdb = new sqlite3.Database('./db/userInfo.db', (err) => {
-      if (err) {
-          console.log("Getting error " + err);
-          exit(1);
-      }
-      createTables(newdb);
+  var newdb = new sqlite3.Database("./db/userInfo.db", (err) => {
+    if (err) {
+      console.log("Getting error " + err);
+      exit(1);
+    }
+    createTables(newdb);
   });
 }
 
@@ -36,7 +39,7 @@ function createTables(newdb) {
     phone_no INTEGER,
     address TEXT,
     logo_filename TEXT
-  );`)
+  );`);
 }
 
 //Registers user to database
@@ -44,12 +47,15 @@ app.post("/register", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   const email = req.body.email;
-  db.run(`INSERT INTO Users (username, password, email) VALUES ("${username}", "${password}", "${email}")`, (err) => {
-    if (err) {
-      console.log(err.message);
+  db.run(
+    `INSERT INTO Users (username, password, email) VALUES ("${username}", "${password}", "${email}")`,
+    (err) => {
+      if (err) {
+        console.log(err.message);
+      }
     }
-  });
-  console.log("Account registered: " + username + " " + password +  " " + email);
+  );
+  console.log("Account registered: " + username + " " + password + " " + email);
   res.redirect("/");
 });
 
@@ -57,24 +63,26 @@ app.post("/register", (req, res) => {
 app.get("/login", (req, res) => {
   const email = req.query.email;
   const password = req.query.password;
-  db.get(`SELECT email, password FROM Users WHERE email = "${email}" AND password = "${password}"`, (err, row) => {
-    if (err) {
-      console.log(err.message)
-    } else {
+  db.get(
+    `SELECT email, password FROM Users WHERE email = "${email}" AND password = "${password}"`,
+    (err, row) => {
+      if (err) {
+        console.log(err.message);
+      } else {
         try {
           if (row.email != null && row.password != null) {
             console.log("Login successful: " + row.email + " " + row.password);
-            } 
-          } catch(err) {
-            console.log("Login failed, mail or password is wrong.");
-            console.log(err.message)
           }
-        } 
+        } catch (err) {
+          console.log("Login failed, mail or password is wrong.");
+          console.log(err.message);
+        }
       }
-  )
+    }
+  );
   res.redirect("/");
 });
 
 const PORT = process.env.PORT || 8080;
-  
+
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
